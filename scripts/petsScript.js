59163ae3cde528574b896a1091914036a7ac66af
Irrisorus <154739805+Irrisorus@users.noreply.github.com>
2024-03-15 18:41:48 +0300

    slider is done except some buttons

--- scripts/petsScript.js
+++ scripts/petsScript.js
@@ -2,6 +2,63 @@ let burgerMenu=document.querySelector(".header__menu-burger");
 let headerMenu=document.querySelector(".header__top-menu")
 let logoTitile=document.querySelector(".logo__title")
 let logoSubtitile=document.querySelector(".logo__subtitle")
+let slidesForSwiper = Array.from(document.querySelectorAll(".swiper-slide"))
+let prevButton= document.querySelector(".button_swiper_left")
+let nextButton= document.querySelector(".button_swiper_rigth")
+let prevEndButton= document.querySelector(".button_swiper_left-end")
+let nextEndButton= document.querySelector(".button_swiper_rigth-end")
+let SwiperButtons=document.querySelectorAll(".button_swiper")
+console.log(slidesForSwiper);
+
+
+slidesForSwiper.forEach((el)=>{
+    if(slidesForSwiper[0].classList.contains("swiper-slide-active")){
+        prevEndButton.classList.add("swiper-button-disabled")
+        console.log("aaa")
+    }
+    else{
+        prevEndButton.classList.remove("swiper-button-disabled")
+    }
+})
+
+// SwiperButtons.forEach((el)=>{
+//     el.addEventListener("click", ()=>{
+//         if(slidesForSwiper[1].classList.contains("swiper-slide-active")){
+//             prevEndButton.classList.add("swiper-button-disabled")
+//             console.log("aaa")
+//         }
+       
+//     })
+// })
+
+new Swiper(".swiper",{
+
+    navigation:{
+
+        nextEl:".button_swiper_rigth",
+        prevEl:".button_swiper_left"
+    },
+    slidesPerView:4,
+    
+
+    breakpoints:{
+        0:{
+            slidesPerView:1,
+            
+        },
+        768:{
+            slidesPerView:2,
+            
+        },
+        1230:{
+            slidesPerView:4,
+           
+        }
+    }
+   
+    
+});
+
 burgerMenu.addEventListener('click',()=>{
   
     console.log("aaaa")
